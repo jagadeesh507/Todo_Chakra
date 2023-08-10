502e6c377de82a9af62faf4573386df0e5ff93a6
Maripi jagadeesh <120298338+jagadeesh507@users.noreply.github.com>
2023-08-10 12:31:56 +0530

    chore:destructor done (#2)

---
+++ pages/TodoCard.js
@@ -0,0 +1,66 @@
+import React from "react";
+import { HStack, Text } from "@chakra-ui/react";
+import { Card, Spacer, IconButton } from "@chakra-ui/react";
+import { CheckIcon, DeleteIcon, EditIcon } from "@chakra-ui/icons";
+
+function TodoCard({ todo, setTodos,setEdited,setEditid,setTodo,setEdit,inputref}) {
+  const { id, title } = todo;
+
+  const handelcomplete = (ids) => {
+    setTodos((prevTodos) =>
+      prevTodos.map((todo) =>
+        todo.id === ids ? { ...todo, isComplete: !todo.isComplete } : todo
+      )
+    );
+  };
+
+  const handeldelete = (id) => {
+    setTodos((prev) => prev.filter((todo) => todo.id !== id));
+  };
+
+  const editTodo = (id, itm) => {
+    setEdit(true);
+    setTodo(itm);
+    setEdited(itm);
+    setEditid(id);
+    inputref.current.focus();
+    handeldelete(id);
+  };
+
+  return (
+    <>
+      <Card
+        mt="4"
+        padding="15px"
+        key={todo.id}
+        overflowWrap="break-word"
+        wordBreak="break-word"
+      >
+        {console.log(todo.id)}
+        <HStack>
+          <Text color={todo.isComplete ? "red" : "black"}>{todo.title}</Text>
+          <Spacer />
+          <IconButton
+            isRound
+            background="pink"
+            icon={<EditIcon />}
+            onClick={() => editTodo(id, title)}
+          />
+          <IconButton
+            isRound
+            background="red"
+            icon={<DeleteIcon />}
+            onClick={() => handeldelete(id)}
+          />
+          <IconButton
+            isRound
+            background="green"
+            icon={<CheckIcon />}
+            onClick={() => handelcomplete(id)}
+          />
+        </HStack>
+      </Card>
+    </>
+  );
+}
+export default TodoCard;
